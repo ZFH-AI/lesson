a788456a73908e92c48f9fd343b4d495a1086d04
fuhai zhang <154525511+ZFH-AI@users.noreply.github.com>
2026-06-27 17:40:42 +0800

    Create c_shell.h

---
+++ operation_ut/tests/ut/framework/include/c_shell.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+#ifndef _OP_API_UT_COMMON_C_SHELL_H_
+#define _OP_API_UT_COMMON_C_SHELL_H_
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int ExecuteShellCmd(const string& cmd, stringstream& ss);
+void SetOpUtSrcPath(const char* p);
+const string& GetOpUtSrcPath();
+const string& GetExePath();
+int GetEnv(const string& name, string& value);
+void SetDebugLog(int debug_log);
+int GetDebugLog();
+#endif
